d2ab7519dfd73bcefb44bb76f9d0009777c597d3
Hritik <46629405+chaudharyhritik@users.noreply.github.com>
2021-04-19 14:55:01 +0530

    add world

--- compile.js
+++ compile.js
@@ -4,3 +4,12 @@ const solc=require('solc');
 const inboxPath=path.resolve(__dirname,'contracts','Star.sol');
 const source = fs.readFileSync(inboxPath,'utf8');
 module.exports=solc.compile(source,1).contracts[':Star'];
+
+
+
+
+
+
+
+
+// hello world hello world
